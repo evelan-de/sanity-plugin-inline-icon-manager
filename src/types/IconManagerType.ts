8d21f07a7227d87cb6b93a970351682ee659f282
Michael Steven David <beltondavid1@gmail.com>
2023-12-15 15:47:05 +0800

    refactor(types): change to use zod schema

--- src/types/IconManagerType.ts
+++ src/types/IconManagerType.ts
@@ -1,41 +1,53 @@
-export type IconManagerColor = {
-  hex: string
-  rgba: {
-    r: number
-    g: number
-    b: number
-    a: number
-  }
-}
+import {z} from 'zod'
 
-export type IconManagerSize = {
-  width: number
-  height: number
-}
+export const iconManagerColorSchema = z.object({
+  hex: z.string(),
+  rgba: z.object({
+    r: z.number(),
+    g: z.number(),
+    b: z.number(),
+    a: z.number(),
+  }),
+})
 
-export type IconManagerType = {
-  icon: string
-  metadata: {
-    url: string
-    downloadUrl: string
-    inlineSvg?: string
-    collectionId: string
-    collectionName: string
-    iconName: string
-    palette?: boolean
-    license?: {
-      name?: string
-      url?: string
-    }
-    author?: {
-      name?: string
-      url?: string
-    }
-    size: IconManagerSize
-    hFlip: boolean
-    vFlip: boolean
-    flip: 'horizontal' | 'vertical' | 'horizontal,vertical'
-    rotate: 0 | 1 | 2 | 3
-    color?: IconManagerColor
-  }
-}
+export type IconManagerColor = z.infer<typeof iconManagerColorSchema>
+
+export const iconManagerSizeSchema = z.object({
+  width: z.number(),
+  height: z.number(),
+})
+
+export type IconManagerSize = z.infer<typeof iconManagerSizeSchema>
+
+export const iconManagerSchema = z.object({
+  icon: z.string(),
+  metadata: z.object({
+    url: z.string(),
+    downloadUrl: z.string(),
+    inlineSvg: z.string().optional(),
+    collectionId: z.string(),
+    collectionName: z.string(),
+    iconName: z.string(),
+    palette: z.boolean().optional(),
+    license: z
+      .object({
+        name: z.string().optional(),
+        url: z.string().optional(),
+      })
+      .optional(),
+    author: z
+      .object({
+        name: z.string().optional(),
+        url: z.string().optional(),
+      })
+      .optional(),
+    size: iconManagerSizeSchema,
+    hFlip: z.boolean(),
+    vFlip: z.boolean(),
+    flip: z.enum(['horizontal', 'vertical', 'horizontal,vertical']),
+    rotate: z.union([z.literal(0), z.literal(1), z.literal(2), z.literal(3)]),
+    color: iconManagerColorSchema.optional(),
+  }),
+})
+
+export type IconManagerType = z.infer<typeof iconManagerSchema>
