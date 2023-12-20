068032d9ac3387108e68a8a8b6fb291a873c9d33
Gian Gonzaga <gian@giangonzaga.com>
2023-12-20 15:46:03 +0000

    chore: initialize i18n in plugin

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,5 +1,6 @@
 import { definePlugin } from 'sanity'
 
+import { deutschBundle, englishBundle } from '../i18n'
 import iconManagerSetup from '../lib/iconManagerSetup'
 import IconManagerObject from '../schemas/objects/IconManager'
 import IconManagerPluginOptions from '../types/IconManagerPluginOptions'
@@ -11,6 +12,9 @@ export const IconManager = definePlugin<void | IconManagerPluginOptions>((config
     name: `sanity-plugin-icon-manager`,
     schema: {
       types: [IconManagerObject(config)],
+    },
+    i18n: {
+      bundles: [englishBundle, deutschBundle],
     },
   }
 })
